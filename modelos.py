ab8ac7143d4afb759c9a308b89a287aa48d18ff2
Felipe Hora <fhora93@gmail.com>
2023-12-11 12:11:39 -0300

    Update modelos.py

--- modelos.py
+++ modelos.py
@@ -1,22 +1,25 @@
 class FundoImobiliario:
-   def __init__(self, codigo, segmento, cotacao_atual, ffo_yield, p_vp, valor_mercado, liquidez, qt_imoveis, preco_m2, aluguel_m2, cap_rate, vacancia_media):
+   def __init__(self, codigo, segmento, cotacao_atual:float,dividend_yield:float, ffo_yield:float, p_vp:float,
+                 valor_mercado:float,liquidez:float, qt_imoveis:float, preco_m2:float,
+                 aluguel_m2:float, cap_rate:float, vacancia_media:float):
        self.codigo = codigo
        self.segmento = segmento
        self.cotacao_atual = cotacao_atual
+       self.dividend_yield = dividend_yield
        self.ffo_yield = ffo_yield
        self.p_vp = p_vp
        self.valor_mercado = valor_mercado
        self.liquidez = liquidez
-       self.qt_imoveis = qt_imoveisss
+       self.qt_imoveis = qt_imoveis
        self.preco_m2 = preco_m2
        self.aluguel_m2 = aluguel_m2
        self.cap_rate = cap_rate
        self.vacancia_media = vacancia_media
 
 class Estrategia:
-    def __init__(segmento="",cotacao_atual_minima=0,ffo_yield_minimo=0,dividend_yield_minimo=0,p_vp_minimo=0,
-                valor_mercado_minimo=0,liquidez_minima=0,qt_minima_imoveis=0,valor_minimo_preco_m2=0,
-                valor_minimo_aluguel_m2=0,valor_minimo_cap_rate=0,maxima_vacancia_media=0):
+    def __init__(self, segmento="",cotacao_atual_minima=0.0,ffo_yield_minimo=0.0,dividend_yield_minimo=0.0,
+                 p_vp_minimo=0.0,valor_mercado_minimo=0.0,liquidez_minima=0.0,qt_minima_imoveis=0.0,
+                 valor_minimo_preco_m2=0.0,valor_minimo_aluguel_m2=0.0,valor_minimo_cap_rate=0.0,maxima_vacancia_media=0.0):
         
         self.segmento = segmento
         self.cotacao_atual_minima = cotacao_atual_minima
@@ -47,10 +50,10 @@ class Estrategia:
                 or fundo.valor_mercado < self.valor_mercado_minimo \
                 or fundo.liquidez < self.liquidez_minima \
                 or fundo.qt_imoveis < self.qt_minima_imoveis \
-                or fundo.valor_preco_m2 < self.valor_minimo_preco_m2 \
-                or fundo.valor_aluguel_m2 < self.valor_minimo_aluguel_m2 \
+                or fundo.preco_m2 < self.valor_minimo_preco_m2 \
+                or fundo.aluguel_m2 < self.valor_minimo_aluguel_m2 \
                 or fundo.cap_rate < self.valor_minimo_cap_rate \
-                or fundo.maxima_vacancia < self.maxima_vacancia_media:
+                or fundo.vacancia_media < self.maxima_vacancia_media:
             return False
         else:
             return True
