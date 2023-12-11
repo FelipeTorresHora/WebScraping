ee8cb5f15e8ab08dd6789b4dd1c2316c9a58e13c
Felipe Hora <fhora93@gmail.com>
2023-12-11 12:11:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 import locale
-from modelos import FundoImobiliario, Esterategia
+from modelos import FundoImobiliario,Estrategia
 
 locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8') 
-def trata_porcentagem(procentagem:str):
+def trata_porcentagem(porcentagem):
     """
     Função para tratar o caracter de porcentagem.
     """
-    return locale.aof(porcentagem.split('%')[0])
+    return locale.atof(porcentagem.split('%')[0])
 
-def trata_decimal(decimal:str):
+def trata_decimal(decimal):
+    """
+    Função para tirar o R$ e o decimal.
+    """
     return locale.atof(decimal.split)
 #header necessário para que o servidor entenda que estamos buscando informações no servidor.
 headers = {"User-Agent" : "Mozilla/5.0"} 
@@ -22,10 +25,10 @@ resultado = []
 estrategia = Estrategia(
     cotacao_atual_minima=50.0,
     dividend_yield_minimo=10,
-    p_vp_minimo=0,70,
+    p_vp_minimo=0.70,
     valor_mercado_minimo=20000000,
     qt_minima_imoveis=5,
-    maxima_Vacancia_media=10
+    maxima_vacancia_media=10
 )
 
 for linha in linhas:
@@ -45,10 +48,9 @@ for linha in linhas:
     cap_rate = trata_porcentagem(dados_fundo[11].text)
     vacancia_media = trata_porcentagem(dados_fundo[12].text)
 
-    fundo_imobiliario = FundoImobiliario(
-        codigo, segmento, cotacao_atual, ffo_yield, p_vp,
-        valor_mercado, liquidez, qt_imoveis, preco_m2, aluguel_m2, cap_rate, vacancia_media
-    )
+    fundo_imobiliario = FundoImobiliario(codigo, segmento, cotacao_atual,dividend_yield, ffo_yield, p_vp,   
+                        valor_mercado,liquidez, qt_imoveis, preco_m2, aluguel_m2, cap_rate, vacancia_media
+                        )
      
     if estrategia.aplica_estrategia(fundo_imobiliario):
         resultado.append(fundo_imobiliario)
